794c35eaf318f270a0112f548912cb5ed744d91c
mraniki <8766259+mraniki@users.noreply.github.com>
2023-03-12 12:23:07 +0100

    Update bot.py

--- src/bot.py
+++ src/bot.py
@@ -97,7 +97,7 @@ async def shutdown_event():
 
 @app.get("/")
 def root():
-    return {f"Bot is online {TTversion}\n{await supertrend_check("BTC/USD", "4h")}"}
+    return {f"Bot is online {TTversion}\n"}
 
 @app.get("/health")
 def health_check():
