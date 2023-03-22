9ff67e6756d90d6833b5b30a157d79461219c7f5
mraniki <8766259+mraniki@users.noreply.github.com>
2023-03-22 17:39:29 +0100

    Update bot.py

--- src/bot.py
+++ src/bot.py
@@ -17,8 +17,6 @@ import apprise
 from apprise import NotifyFormat
 
 from microdot import Microdot
-from microdot_asyncio import Microdot, Response
-from microdot_utemplate import render_template
 #twelvedata.com
 from twelvedata import TDClient
 import time
@@ -124,17 +122,34 @@ def checker():
         logger.info(msg=f"symboltrend {symboltrend}")
 
 
+
 app = Microdot()
-Response.default_content_type = 'text/html'
+
+htmldoc = '''<!DOCTYPE html>
+<html>
+    <head>
+        <title>Microdot Example Page</title>
+    </head>
+    <body>
+        <div>
+            <h1>Microdot Example Page</h1>
+            <p>Hello from Microdot!</p>
+            <p><a href="/shutdown">Click to shutdown the server</a></p>
+        </div>
+    </body>
+</html>
+'''
 
 
-@app.route('/', methods=['GET', 'POST'])
-async def index(req):
-    name = None
-    if req.method == 'POST':
-        name = req.form.get('name')
-    return render_template('index.html', name=name)
+@app.route('/')
+def hello(request):
+    return htmldoc, 200, {'Content-Type': 'text/html'}
 
 
-if __name__ == '__main__':
-    app.run()
+@app.route('/shutdown')
+def shutdown(request):
+    request.app.shutdown()
+    return 'The server is shutting down...'
+
+
+app.run(debug=True)
