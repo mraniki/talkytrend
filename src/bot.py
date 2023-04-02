4bcce5b6ddaa6adea0dc937026227f7ce21c9148
eridu <8766259+mraniki@users.noreply.github.com>
2023-04-02 15:32:49 +0200

    update

--- src/bot.py
+++ src/bot.py
@@ -8,12 +8,15 @@ import pandas as pd
 
 load_dotenv()
 GSHEET=os.getenv("GSHEET")
+PORT=os.getenv("PORT", "8080")
+HOST=os.getenv("HOST", "0.0.0.0")
 
 def get_dashboard():
     gc = gspread.service_account(filename='config/service_account.json')
     book = gc.open_by_key(GSHEET)
     dashboard = book.get_worksheet(0)
     df = pd.DataFrame(dashboard.get_all_values())
+    print(df)
     return df
 
 app = FastAPI()
@@ -23,4 +26,4 @@ def read_root():
     return  get_dashboard()
 
 if __name__ == '__main__':
-    uvicorn.run("bot:app", host="localhost", port=8080, reload=True)
+    uvicorn.run("bot:app", host=HOST, port=PORT, reload=True)
