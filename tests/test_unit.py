e6cdc5e7a1d82bfdc2347d3026b5fd963a474232
mraniki <mraniki@users.noreply.github.com>
2023-06-07 22:02:23 +0200

    :white_check_mark: Refactor test_fetch_analysis function in test_unit.py

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -16,23 +16,24 @@ def trend():
 @pytest.mark.asyncio
 async def test_fetch_analysis():
     # Mock the TA_Handler class
-    with patch("tradingview_ta.TA_Handler") as mock_handler: 
+   # with patch("tradingview_ta.TA_Handler") as mock_handler: 
         # Mock the get_analysis method to return a predefined result
-        mock_handler.return_value.get_analysis.return_value = {
-            "summary": {"RECOMMENDATION": "BUY"}
+        #mock_handler.return_value.get_analysis.return_value = {
+           # "summary": {"RECOMMENDATION": "BUY"}
         }
 
         # Initialize the plugin with 'EURUSD'
-        plugin = TrendPlugin("EURUSD")
+    plugin = TrendPlugin("EURUSD")
 
         # Call the fetch_analysis method
-        result = await plugin.fetch_analysis()
+    result = await plugin.fetch_analysis()
+    print(result)
 
         # Check that the method called the correct methods on the TA_Handler
-        mock_handler.return_value.set_symbol_as.assert_called_with("EURUSD")
+        #mock_handler.return_value.set_symbol_as.assert_called_with("EURUSD")
         #mock_handler.return_value.set_screener_as.assert_called_with("forex")
         #mock_handler.return_value.set_interval_as.assert_called_with(Interval.INTERVAL_15_MINUTES)
         #mock_handler.return_value.get_analysis.assert_called()
 
         # Check that the method returned the correct result
-        assert result == "BUY"
+    assert result == "BUY"
