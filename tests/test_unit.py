63d445f91d99c5d55baaf4c3541ed8ee21a3c959
mraniki <8766259+mraniki@users.noreply.github.com>
2023-06-08 13:18:46 +0200

    :zap: Update test_unit.py

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -15,51 +15,17 @@ def trend():
 
 @pytest.mark.asyncio
 async def test_fetch_analysis():
-    # Mock the TA_Handler class
-   # with patch("tradingview_ta.TA_Handler") as mock_handler: 
-        # Mock the get_analysis method to return a predefined result
-        #mock_handler.return_value.get_analysis.return_value = {
-           # "summary": {"RECOMMENDATION": "BUY"}
-        
-
-        # Initialize the plugin with 'EURUSD'
-    plugin = TrendPlugin(asset="EURUSD")
+    plugin = TrendPlugin()
     print(plugin)
-        # Call the fetch_analysis method
     result = await plugin.fetch_analysis()
     print(result)
-
-        # Check that the method called the correct methods on the TA_Handler
-        #mock_handler.return_value.set_symbol_as.assert_called_with("EURUSD")
-        #mock_handler.return_value.set_screener_as.assert_called_with("forex")
-        #mock_handler.return_value.set_interval_as.assert_called_with(Interval.INTERVAL_15_MINUTES)
-        #mock_handler.return_value.get_analysis.assert_called()
-
-        # Check that the method returned the correct result
     assert result is not None
  
 
 @pytest.mark.asyncio
 async def test_fetch_analysis_crypto():
-    # Mock the TA_Handler class
-   # with patch("tradingview_ta.TA_Handler") as mock_handler: 
-        # Mock the get_analysis method to return a predefined result
-        #mock_handler.return_value.get_analysis.return_value = {
-           # "summary": {"RECOMMENDATION": "BUY"}
-        
-
-        # Initialize the plugin with 'EURUSD'
     plugin = TrendPlugin(asset='BTCUSDT',exchange='Binance',screener='crypto',interval=Interval.INTERVAL_15_MINUTES)
     print(plugin)
-        # Call the fetch_analysis method
     result = await plugin.fetch_analysis()
     print(result)
-
-        # Check that the method called the correct methods on the TA_Handler
-        #mock_handler.return_value.set_symbol_as.assert_called_with("EURUSD")
-        #mock_handler.return_value.set_screener_as.assert_called_with("forex")
-        #mock_handler.return_value.set_interval_as.assert_called_with(Interval.INTERVAL_15_MINUTES)
-        #mock_handler.return_value.get_analysis.assert_called()
-
-        # Check that the method returned the correct result
     assert result is not None
