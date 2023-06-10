0f70d7c2332eb9c24e2edd6a708934fbec71fcc7
mraniki <8766259+mraniki@users.noreply.github.com>
2023-06-10 17:23:45 +0200

    ♻️ refactor

--- talkytrend/main.py
+++ talkytrend/main.py
@@ -12,52 +12,45 @@ from talkytrend import __version__
 from .config import settings
 
 class TalkyTrend:
-    def __init__(self,
-                 asset=None,
-                 exchange="FX_IDC",
-                 screener="forex",
-                 interval=Interval.INTERVAL_4_HOURS):
-        self.logger = logging.getLogger(name="TalkyTrend")
-        if asset is None:
-            asset = settings.asset
+    def __init__(self, assets=settings.assets):
+        self.economic_calendar = settings.economic_calendar
+        self.news_url = f"{settings.news_url}{settings.news_api_key}" if settings.news_api_key else None
         self.enabled = settings.talkytrend_status
-        self.asset = asset
-        self.exchange = exchange
-        self.screener = screener
-        self.interval = interval
-        self.asset_signals = {'4h': None} 
+        self.assets = assets
+        self.asset_signals = {"15m": None, "1h": None, "4h": None}
 
     async def fetch_analysis(self):
-        
-        # Initialize the TA_Handler
-        handler = TA_Handler(
-        symbol=self.asset,
-        exchange=self.exchange,
-        screener=self.screener,
-        interval=self.interval)
+        recommendations = []
+        for asset in self.assets:
+            handler = TA_Handler(
+                symbol=asset["id"],
+                exchange=asset["exchange"],
+                screener=asset["screener"],
+                interval=asset["interval"]
+            )
+            analysis = handler.get_analysis()
+            recommendations.append(analysis.summary["RECOMMENDATION"])
+        return recommendations
 
-        analysis = handler.get_analysis()
-        print(analysis)
-        return analysis.summary['RECOMMENDATION']
 
     async def check_signal(self):
-        current_signal = await self.fetch_analysis()
-        if self.asset_signals[self.interval] and current_signal != self.asset_signals[self.interval]:
-            message = f'New signal for {self.asset} ({self.interval}): {current_signal}'
-            print(message)
-            self.asset_signals[self.interval] = current_signal
-            return message
-            
-    async def monitor_assets(self):
-        while True:
-            await asyncio.gather(*[self.check_signal()])
-            await asyncio.sleep(settings.scanner_frequency)
+        messages = []
+        for asset in self.assets:
+            current_signal = await self.fetch_analysis(asset["id"], asset["interval"])
+            if self.asset_signals.get(asset["id"]) and self.asset_signals[asset["id"]].get(asset["interval"]) and current_signal != self.asset_signals[asset["id"]][asset["interval"]]:
+                message = f'New signal for {asset["id"]} ({asset["interval"]}): {current_signal}'
+                print(message)
+                self.asset_signals[asset["id"]][asset["interval"]] = current_signal
+                messages.append(message)
+            elif not self.asset_signals.get(asset["id"]):
+                self.asset_signals[asset["id"]] = {asset["interval"]: current_signal}
+            else:
+                self.asset_signals[asset["id"]][asset["interval"]] = current_signal
+        return messages
 
-class TalkyBreaking:
-    def __init__(self):
-        self.logger = logging.getLogger(name="TalkyBreaking")
-        self.economic_calendar = settings.economic_calendar
-        self.news_url = f"{settings.news_url}{settings.news_api_key}"
+
+    def get_asset_signals(self):
+        return self.asset_signals
 
     async def fetch_key_events(self):
         response = requests.get(self.economic_calendar, timeout=10)  
@@ -79,8 +72,16 @@ class TalkyBreaking:
         for article in articles:
             print("Title: ", article['title'])
             print("Description: ", article['description'])
+            return article
 
-    async def monitor_events(self):
+    async def scanner(self):
         while True:
-            await asyncio.gather(*[self.fetch_key_events()])
+            tasks = [self.check_signal()]
+            tasks.append(self.fetch_key_events())
+            tasks.append(self.fetch_key_news())
+            results = await asyncio.gather(*tasks)
+            if results[1] is not None:
+                print("Key event:", results[1])
+            if results[2] is not None:
+                print("Key news:", results[2]["title"])
             await asyncio.sleep(settings.scanner_frequency)
