d93df5b37b9e069bf35da9ae129b3ad228f5be34
mraniki <mraniki@users.noreply.github.com>
2023-06-22 10:24:59 +0200

    :memo: docs

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,11 @@ author = 'mraniki'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon'
+    ]
 
 templates_path = ['_templates']
 exclude_patterns = ['build', 'Thumbs.db', '.DS_Store']
