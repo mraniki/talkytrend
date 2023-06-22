49b8127e1ac2850d843a423c9706a408e84b004c
mraniki <8766259+mraniki@users.noreply.github.com>
2023-06-22 06:19:00 +0200

    💬 doc

--- docs/docs/conf.py
+++ docs/docs/conf.py
@@ -13,23 +13,15 @@ author = 'mraniki'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.napoleon'
-]
+extensions = []
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-language = "en"
+
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-
-html_theme = "furo"
-html_static_path = ["_static"]
-add_module_names = False
-autoapi_python_class_content = "both"
-python_use_unqualified_type_names = True
+html_theme = 'alabaster'
+html_static_path = ['_static']
