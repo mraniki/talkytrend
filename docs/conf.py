ee9e7a236359f92564b618b18a36e5aecf217888
mraniki <8766259+mraniki@users.noreply.github.com>
2023-07-03 19:10:04 +0200

    📝  Doc config

--- docs/conf.py
+++ docs/conf.py
@@ -16,19 +16,18 @@ author = 'mraniki'
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
-    'sphinx.ext.napoleon'
-    ]
+    'sphinx.ext.napoleon',
+    'sphinx.ext.intersphinx'
+]
 
 templates_path = ['_templates']
-exclude_patterns = ['build', 'Thumbs.db', '.DS_Store']
-
+exclude_patterns = []
+master_doc = 'index'
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "furo"
-html_static_path = ["_static"]
-add_module_names = False
-autoapi_python_class_content = "both"
-python_use_unqualified_type_names = True
+html_static_path = ['_static']
+
