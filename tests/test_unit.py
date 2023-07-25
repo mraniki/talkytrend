e792dbf343732b5ec049112dd56260580e0119d1
mraniki <8766259+mraniki@users.noreply.github.com>
2023-07-25 09:35:44 +0200

    ✅

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -85,6 +85,6 @@ async def test_monitor(talky):
     result = await talky.monitor()
     print(result)
     assert result is not None
-    # assert "💬" in result 
-    # assert "📰" in result 
-    # assert "EURUSD" in result 
+    assert any("💬" in item for item in result)
+    assert any("📰" in item for item in result)
+    assert any("EURUSD" in item for item in result)
