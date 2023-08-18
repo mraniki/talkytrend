be9dff841c9a3bf0a7ca1bb1c0bb97cbe7686dd1
mraniki <mraniki@users.noreply.github.com>
2023-08-18 19:25:53 +0200

    :bug: settings name

--- talkytrend/main.py
+++ talkytrend/main.py
@@ -321,7 +321,7 @@ class TalkyTrend:
             "tbm": "nws",  # news
             "tbs": "qdr:d", # last 24h
             "num": 1,
-            "api_key": settings.llm_serpapi_key})
+            "api_key": settings.serpapi_key})
         data = search.get_dict()
         return data['news_results'][0]['link']
             
