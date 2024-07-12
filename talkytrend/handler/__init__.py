04e5fb968ebb06ea9fe924e2867f86707cb01307
mraniki <mraniki@users.noreply.github.com>
2024-07-12 16:49:42 +0200

    Refactor handler imports in __init__.py file

--- talkytrend/handler/__init__.py
+++ talkytrend/handler/__init__.py
@@ -1,9 +1,9 @@
-from .handler.calendar import CalendarHandler
-from .handler.feed import FeedHandler
-from .handler.livetv import LivetvHandler
-from .handler.scraper import ScraperHandler
-from .handler.tradingview import TradingviewHandler
-from .handler.yfinance import YfinanceHandler
+from talkytrend.handler.calendar import CalendarHandler
+from talkytrend.handler.feed import FeedHandler
+from talkytrend.handler.livetv import LivetvHandler
+from talkytrend.handler.scraper import ScraperHandler
+from talkytrend.handler.tradingview import TradingviewHandler
+from talkytrend.handler.yfinance import YfinanceHandler
 
 # from .handler.finnhub import FinnhubHandler
 # from .handler.cot import CotHandler
